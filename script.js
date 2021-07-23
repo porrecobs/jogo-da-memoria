10e084d9ec816e8c0a628d5b6bafb5f6e84bd7a1
nico pereira <nicolaswbpereira@gmail.com>
2021-07-23 00:36:02 -0300

    adicionando opcao de reiniciar o jogo

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var primeiraCarta, segundaCarta;
 
 var quantidadeCartoes = window.innerWidth >= 600 ? 6 : 6;
 
-console.log(window.innerWidth);
+var pontuacao = 0;
 
 var displayText = document
   .getElementById("containerText")
@@ -57,6 +57,7 @@ cartasSelecionadas.forEach((item) => {
   item.classList.remove("hide");
   var clone = item.cloneNode(true);
   clone.style.order = Math.floor(Math.random() * quantidadeCartoes);
+  clone.classList.add("clone");
   document.getElementById("jogo").appendChild(clone);
 });
 
@@ -75,12 +76,18 @@ function comparaCarta() {
 }
 
 function desabilitarCartas() {
-  primeiraCarta.removeEventListener("click", virarCarta);
-  segundaCarta.removeEventListener("click", virarCarta);
-  setTimeout(() => {
-    limparValores();
-  }, 1000);
-  return;
+  if (pontuacao >= 5) {
+    setTimeout(() => { fimDeJogo()}, 3000);
+  } else {
+    pontuacao = pontuacao + 1;
+    primeiraCarta.removeEventListener("click", virarCarta);
+    segundaCarta.removeEventListener("click", virarCarta);
+    segundaCarta.classList.remove("segunda-carta");
+    setTimeout(() => {
+      limparValores();
+    }, 3000);
+    return;
+  }
 }
 
 function desvirarCartas() {
@@ -104,10 +111,79 @@ function limparValores() {
 function iniciarJogo() {
   var jogo = document.getElementById("jogo-container");
   var texto = document.getElementById("textoInicial");
+  var fim = document.getElementById("fim-container");
 
   jogo.classList.remove("hide");
+  fim.classList.add("hide");
+  fim.style.display = "none";
   texto.classList.add("hide");
   texto.style.display = "none";
+
+  pontuacao = 0;
+  
   window.scrollTo(0, 0);
   setTimeout(() => {}, 500);
+}
+
+function fimDeJogo() {
+  var fim = document.getElementById("fim-container");
+  
+  fim.classList.remove("hide");
+  fim.style.display = "block";
+}
+
+function reiniciarJogo() {
+  cartasSelecionadas.forEach((item) => {
+    console.log(item);
+    item.classList.add("hide");
+    item.classList.remove("flip");
+    item.classList.remove("segunda-carta");
+  });
+  
+  var cloneList = document.getElementsByClassName("clone");
+  console.log('cloneList', cloneList);
+  
+  for (var i = 0; i < 6;) {
+    if (cloneList.length > 0) {
+      cloneList[0].classList.add("hide");
+      cloneList[0].classList.remove("flip");
+      cloneList[0].classList.remove("segunda-carta");
+      document.getElementById("jogo").removeChild(cloneList[0]);
+    }
+    i++;
+  }
+
+  cartasSelecionadas = [];
+  limparValores();
+
+  cards.forEach((item) => {
+    var posAleatoria = Math.floor(Math.random() * 6);
+    item.style.order = posAleatoria;
+  });
+
+  for (var i = 0; i < quantidadeCartoes; ) {
+    var aux = cards[Math.floor(Math.random() * 20)];
+    if (cartasSelecionadas.indexOf(aux) < 0) {
+      cartasSelecionadas.push(aux);
+      i++;
+    }
+  }
+
+  cartasSelecionadas.forEach((item) => {
+    item.classList.remove("hide");
+    var clone = item.cloneNode(true);
+    clone.classList.add("clone");
+    clone.style.order = Math.floor(Math.random() * quantidadeCartoes);
+    document.getElementById("jogo").appendChild(clone);
+  });
+
+  document.getElementById("jogo").childNodes.forEach((item) => {
+    item.addEventListener("click", virarCarta);
+  });
+
+  iniciarJogo();
+}
+
+function irParaGaleria() {
+  window.location.href = "./gallery.html";
 }
